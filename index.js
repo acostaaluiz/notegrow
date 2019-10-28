ce501e486c166c1baea2464eabc099ce2b53697d
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2019-10-28 14:50:15 -0300

    styling errors

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+// eslint-disable-next-line import/extensions
 import App from './src/App.tsx';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
