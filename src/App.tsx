553ff2fa55721ac416d8a8e287911e64908eac6b
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2019-10-31 15:30:04 -0300

    Removed themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ReduxNetworkProvider } from 'react-native-offline';
-import { ThemeProvider } from 'styled-components';
 import { store, persisted } from './store';
 import Router from './router';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ErrorBoundary } from './components/utils';
-import themes from './styles';
 
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider theme={themes}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persisted}>
-            <ReduxNetworkProvider>
-              <Router />
-            </ReduxNetworkProvider>
-          </PersistGate>
-        </Provider>
-      </ThemeProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persisted}>
+          <ReduxNetworkProvider>
+            <Router />
+          </ReduxNetworkProvider>
+        </PersistGate>
+      </Provider>
     </ErrorBoundary>
   );
 }
