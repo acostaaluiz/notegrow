8d0c4dd15da0a2ef875a7257491cbb00ca5b5bee
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2019-11-01 14:43:41 -0300

    Fixed android fonts issue

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,69 +1,69 @@
 export const fonts = {
-  votorantim: 'VotorantimSans-Regular',
-  roboto: 'Roboto-Regular'
+  votorantim: {
+    regular: 'VotorantimSans-Regular',
+    medium: 'VotorantimSans-Medium',
+    bold: 'VotorantimSans-Bold',
+  },
+  roboto: {
+    regular: 'Roboto-Regular',
+    italic: 'Roboto-Italic',
+    medium: 'Roboto-Medium',
+    mediumitalic: 'Roboto-MediumItalic',
+  }
 }
 
 export const styledfont = {
   h1: `
-    font-family: ${fonts.votorantim};
+    font-family: ${fonts.votorantim.bold};
     font-size: 56px;
-    font-weight: bold;
     letter-spacing: -0.8;
     line-height: 70px;
   `,
   h2: `
-    font-family: ${fonts.votorantim};
+    font-family: ${fonts.votorantim.bold};
     font-size: 32;
-    font-weight: bold;
     letter-spacing: -1.5;
     line-height: 40px;
   `,
   h3: `
-    font-family: ${fonts.votorantim};
+    font-family: ${fonts.votorantim.bold};
     font-size: 24;
-    font-weight: bold;
     letter-spacing: -1;
     line-height: 28px;
   `,
   h4: `
-    font-family: ${fonts.votorantim};
+    font-family: ${fonts.votorantim.regular};
     font-size: 24;
-    font-weight: regular;
     letter-spacing: -1;
     line-height: 28px;
   `,
   h5: `
-    font-family: ${fonts.votorantim};
+    font-family: ${fonts.votorantim.medium};
     font-size: 16;
-    font-weight: medium;
     letter-spacing: -0.5;
     line-height: 20px;
   `,
   subtitle1: `
-    font-family: ${fonts.roboto};
+    font-family: ${fonts.roboto.regular};
     font-size: 16;
-    font-weight: regular;
     letter-spacing: 1.5;
     line-height: 24px;
   `,
   subtitle2: `
-    font-family: ${fonts.roboto};
+    font-family: ${fonts.roboto.medium};
     font-size: 14;
-    font-weight: regular;
     letter-spacing: 0.1;
     line-height: 24px;
   `,
   body1: `
-    font-family: ${fonts.roboto};
+    font-family: ${fonts.roboto.regular};
     font-size: 16;
-    font-weight: regular;
     letter-spacing: 0.5;
     line-height: 24px;
   `,
   body2: `
-    font-family: ${fonts.roboto};
+    font-family: ${fonts.roboto.regular};
     Font-size: 16
-    font-weight: regular;
     letter-spacing: 0.25
     line-height: 20px;
   `,
