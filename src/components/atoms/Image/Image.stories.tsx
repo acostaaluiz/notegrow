c0e084c46c07c83f5dac6eee73eb629288c9667d
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2019-11-05 15:20:49 -0300

    Created Logo image

--- src/components/atoms/Image/Image.stories.tsx
+++ src/components/atoms/Image/Image.stories.tsx
@@ -3,20 +3,29 @@ import { storiesOf } from '@storybook/react-native';
 import { withKnobs, select } from '@storybook/addon-knobs';
 import Image, { ImagesNames, ImageNamesType } from './Image';
 import CenterView from '../../../utils/Storybook/CenterView';
+import { ThemeProvider } from 'styled-components';
 
 const stories = storiesOf('Images', module);
 
 stories.addDecorator(withKnobs);
 
-const useKnobs = (selected = ImagesNames[0]): [ImageNamesType] => {
+const useKnobs = (
+  selected: ImageNamesType = ImagesNames[0],
+): [ImageNamesType] => {
   return [select('name', ImagesNames, selected)];
 };
 
-stories.add('Abstract1', () => {
-  const [name] = useKnobs();
+const render = (selected: ImageNamesType) => () => {
+  const [name] = useKnobs(selected);
   return (
-    <CenterView>
-      <Image name={name} />
-    </CenterView>
+    <ThemeProvider theme={{ dark: selected === 'logo-white' }}>
+      <CenterView>
+        <Image name={name} />
+      </CenterView>
+    </ThemeProvider>
   );
+};
+
+ImagesNames.forEach((name: ImageNamesType) => {
+  stories.add(name as string, render(name));
 });
--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Abstract1 from './Abstract1';
+import Logo from './Logo';
 
-export const ImagesNames = ['abstract1'] as const;
+export const ImagesNames = [
+  'abstract1',
+  'logo',
+  'logo-secondary',
+  'logo-white',
+] as const;
 
 // Creates dynamic type from array
 export type ImageNamesType = typeof ImagesNames[number];
@@ -14,6 +20,12 @@ function Image({ name }: ImageProps) {
   switch (name) {
     case 'abstract1':
       return <Abstract1 />;
+    case 'logo':
+      return <Logo />;
+    case 'logo-secondary':
+      return <Logo type="secondary" />;
+    case 'logo-white':
+      return <Logo type="white" />;
     default:
       return null;
   }
