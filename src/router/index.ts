42390bc4ca256bd33439b8762f9acea0c66a8003
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2019-11-08 17:11:44 -0300

    Simplified routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,56 +14,62 @@ import SignUpPasswordScreen from '../scenes/SignUpPasswordScreen';
 import SignUpPasswordConfirmationScreen from '../scenes/SignUpPasswordConfirmationScreen';
 import SignUpCompleteScreen from '../scenes/SignUpCompleteScreen';
 
-const HomeNavigator = createBottomTabNavigator({
-  Home: {
-    screen: HomeScreen
+const HomeNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Screen2: {
+      screen: HomeScreen,
+    },
+    Preferences: {
+      screen: UserPreferences,
+      navigationOptions: {
+        tabBarLabel: 'Preferências',
+      },
+    },
   },
-  Screen2: {
-    screen: HomeScreen,
-  },
-  Preferences: {
-    screen: UserPreferences,
-    navigationOptions: {
-      tabBarLabel: 'Preferências'
-    }
-  },
-}, { initialRouteName: 'Home' })
+  { initialRouteName: 'Home' },
+);
 
-const SignUpNavigation = createStackNavigator({
-  SignUp: SignUpBlueScreen,
-  SignUpForm: SignUpFormScreen,
-  SignUpPassword: SignUpPasswordScreen,
-  SignUpPasswordConfirmation: SignUpPasswordConfirmationScreen,
-  SignUpComplete: SignUpCompleteScreen,
-}, {
-  initialRouteName: 'SignUp',
-  ...noHeader
-})
+const SignUpNavigation = createStackNavigator(
+  {
+    SignUp: SignUpBlueScreen,
+    SignUpForm: SignUpFormScreen,
+    SignUpPassword: SignUpPasswordScreen,
+    SignUpPasswordConfirmation: SignUpPasswordConfirmationScreen,
+    SignUpComplete: SignUpCompleteScreen,
+  },
+  {
+    initialRouteName: 'SignUp',
+    ...noHeader,
+  },
+);
 
-const LoginNavigator = createStackNavigator({
-  Login: {
-    screen: LoginScreen,
+const LoginNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    LoginPassword: LoginPasswordScreen,
+    SignUp: SignUpNavigation,
   },
-  LoginPassword: {
-    screen: LoginPasswordScreen
+  {
+    initialRouteName: 'Login',
+    ...noHeader,
   },
-  SignUp: SignUpNavigation
-}, {
-  initialRouteName: 'Login',
-  ...noHeader
-});
+);
 
 const FAQNavigator = createStackNavigator({
-  FAQ: {
-    screen: FAQScreen,
-  }
+  FAQ: FAQScreen,
 });
 
-const AppNavigator = createSwitchNavigator({
-  AppOpened: AppOpened,
-  Login: LoginNavigator,
-  Home: HomeNavigator,
-  FAQ: FAQNavigator
-}, { initialRouteName: 'Login' });
+const AppNavigator = createSwitchNavigator(
+  {
+    AppOpened: AppOpened,
+    Login: LoginNavigator,
+    Home: HomeNavigator,
+    FAQ: FAQNavigator,
+  },
+  { initialRouteName: 'Login' },
+);
 
 export default createAppContainer(AppNavigator);
